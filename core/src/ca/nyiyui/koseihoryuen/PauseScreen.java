f222a1183d332c306c35943bd1519ca5f757ae91
nyiyui <+@nyiyui.ca>
2023-05-19 13:25:08 -0400

    pausing and stuff i guess

--- core/src/ca/nyiyui/koseihoryuen/PauseScreen.java
+++ core/src/ca/nyiyui/koseihoryuen/PauseScreen.java
@@ -1,9 +1,12 @@
 package ca.nyiyui.koseihoryuen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
 
-public class PauseScreen extends ScreenAdapter2{
+public class PauseScreen extends ScreenAdapter2 implements PlayableScreen{
+    private PlayScreen playScreen;
     public PauseScreen(Koseihoryuen game) {
         super(game);
     }
@@ -12,5 +15,29 @@ public class PauseScreen extends ScreenAdapter2{
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 1, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(new InputAdapter() {
+            @Override
+            public boolean keyDown(int keycode) {
+                switch (keycode) {
+                    case Input.Keys.ESCAPE:
+                        playScreen.invokePlay();
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void hide() {
+        Gdx.input.setInputProcessor(null);
+    }
+
+    @Override
+    public void setPlayScreen(PlayScreen ps) {
+        playScreen=ps;
     }
 }
