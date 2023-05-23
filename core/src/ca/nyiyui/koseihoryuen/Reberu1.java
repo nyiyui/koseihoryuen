0866f3bcac10fe45427bf9a87bac693a267e840c
nyiyui <+@nyiyui.ca>
2023-05-23 12:43:34 -0400

    partial yes

--- core/src/ca/nyiyui/koseihoryuen/Reberu1.java
+++ core/src/ca/nyiyui/koseihoryuen/Reberu1.java
@@ -8,6 +8,8 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,6 +17,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 
 public class Reberu1 extends ScreenAdapter2 implements PlayableScreen {
+    private Box2DDebugRenderer debugRenderer;
     private Stage stage;
     private PlayScreen playScreen;
     private Daishi daishi;
@@ -26,7 +29,18 @@ public class Reberu1 extends ScreenAdapter2 implements PlayableScreen {
     private Texture pathway;
     private Music music;
     private Telop telop;
-    private boolean exploring = false;
+    private final static int STATE_INST = 1;
+    private final static int STATE_EXPLORE = 2;
+    private final static int STATE_COMPLETE = 3;
+    private int state = STATE_INST;
+    private World world;
+    /**
+     * Frame time accumulator for Box2D timestep.
+     * Used to specify a max delta time for physics simulation
+     */
+    private float box2dFrameAccum = 0;
+    private Body ballBody;
+    private Body player;
 
     Reberu1(Koseihoryuen game) {
         super(game);
@@ -40,6 +54,63 @@ public class Reberu1 extends ScreenAdapter2 implements PlayableScreen {
         switchLine(0);
         background = new Texture(Gdx.files.internal("images/stage1-bg.png"));
         pathway = new Texture(Gdx.files.internal("images/stage1-pathway.png"));
+        setupBox2D();
+    }
+
+    private void setupBox2D() {
+        world = new World(new Vector2(0, 10), true);
+        debugRenderer = new Box2DDebugRenderer();
+        debugRenderer.setDrawAABBs(true);
+
+        // First we create a body definition
+        BodyDef bodyDef = new BodyDef();
+// We set our body to dynamic, for something like ground which doesn't move we would set it to StaticBody
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+// Set our body's starting position in the world
+        bodyDef.position.set(10, 100);
+
+// Create our body in the world using our body definition
+        ballBody = world.createBody(bodyDef);
+
+// Create a circle shape and set its radius to 6
+        CircleShape circle = new CircleShape();
+        circle.setRadius(6f);
+
+// Create a fixture definition to apply our shape to
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = circle;
+        fixtureDef.density = 0.5f;
+        fixtureDef.friction = 0.4f;
+        fixtureDef.restitution = 0.6f; // Make it bounce a little bit
+
+// Create our fixture and attach it to the body
+        Fixture fixture = ballBody.createFixture(fixtureDef);
+
+// Remember to dispose of any shapes after you're done with them!
+// BodyDef and FixtureDef don't need disposing but shapes do.
+        circle.dispose();
+
+        BodyDef groundBodyDef = new BodyDef();
+        groundBodyDef.position.set(new Vector2(0, 0));
+        Body groundBody = world.createBody(groundBodyDef);
+        PolygonShape groundBox = new PolygonShape();
+        groundBox.setAsBox(game.camera.viewportWidth/2, game.camera.viewportHeight/2);
+        groundBody.createFixture(groundBox, 0.0f);
+        groundBox.dispose();
+
+        bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.KinematicBody;
+        bodyDef.position.set(10, 400);
+        player = world.createBody(bodyDef);
+        circle = new CircleShape();
+        circle.setRadius(50f);
+        fixtureDef = new FixtureDef();
+        fixtureDef.shape = circle;
+        fixtureDef.density = 0.5f;
+        fixtureDef.friction = 0.5f;
+        fixtureDef.restitution = 0.6f;
+        fixture = player.createFixture(fixtureDef);
+        circle.dispose();
     }
 
     @Override
@@ -102,7 +173,7 @@ public class Reberu1 extends ScreenAdapter2 implements PlayableScreen {
         if (cl.action != null)
             switch (cl.action) {
                 case "explore":
-                    exploring = true;
+                    state = STATE_EXPLORE;
                     break;
             }
     }
@@ -111,13 +182,37 @@ public class Reberu1 extends ScreenAdapter2 implements PlayableScreen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        renderBox2D(delta);
         game.batch.begin();
-        game.batch.draw(background, 0, 0);
-        if (exploring)
-            game.batch.draw(pathway, 0, 0);
+//        game.batch.draw(background, 0, 0);
+        if (state == STATE_EXPLORE) {
+//            game.batch.draw(pathway, 0, 0);
+            Vector2 pos = ballBody.getPosition();
+            Vector2 vel = ballBody.getLinearVelocity();
+            if (Gdx.input.isKeyPressed(Input.Keys.A))
+                ballBody.setLinearVelocity(-1.0f,0f);
+            if (Gdx.input.isKeyPressed(Input.Keys.D))
+                ballBody.setLinearVelocity(1.0f,0f);
+//                ballBody.applyForce(100.0f, 0.0f, pos.x, pos.y, true);
+        }
         if (curLine().body != null && !curLine().body.equals(""))
             telop.draw(game.batch, 0, 0, game.camera.viewportWidth, 200);
+        Vector2 pos = ballBody.getPosition();
+        renderText(game.debugFont, "ball", pos.x, pos.y);
+        pos=player.getPosition();
+        renderText(game.debugFont, "player", pos.x, pos.y);
         game.batch.end();
+        debugRenderer.render(world, game.camera.combined);
+    }
+
+    private void renderBox2D(float delta) {
+        float frameTime = Math.min(delta, 0.25f);
+        box2dFrameAccum += frameTime;
+        while (box2dFrameAccum >= Settings.BOX2D_TIME_STEP) {
+            world.step(Settings.BOX2D_TIME_STEP, Settings.BOX2D_VELOCITY_ITERATIONS, Settings.BOX2D_POSITION_ITERATIONS);
+            box2dFrameAccum -= Settings.BOX2D_TIME_STEP;
+        }
+        debugRenderer.render(world, game.camera.combined);
     }
 
     public Daishi getDaishi() {
@@ -126,5 +221,11 @@ public class Reberu1 extends ScreenAdapter2 implements PlayableScreen {
 
     public void setDaishi(Daishi daishi) {
         this.daishi = daishi;
+    }
+
+    @Override
+    public void dispose() {
+        debugRenderer.dispose();
+        world.dispose();
     }
 }
---
+++ core/src/ca/nyiyui/koseihoryuen/Settings.java
@@ -0,0 +1,7 @@
+package ca.nyiyui.koseihoryuen;
+
+public class Settings {
+    public static float BOX2D_TIME_STEP = 1/60f;
+    public static int BOX2D_VELOCITY_ITERATIONS = 8;
+    public static int BOX2D_POSITION_ITERATIONS = 3;
+}
