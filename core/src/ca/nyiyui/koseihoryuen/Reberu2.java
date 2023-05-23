f38a9bd2b33f94bbfeb99bab65db1f9bbd24b0be
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-05-23 13:27:33 -0400

    copied and pasted daishi stuff from reberu1

--- core/src/ca/nyiyui/koseihoryuen/Reberu2.java
+++ core/src/ca/nyiyui/koseihoryuen/Reberu2.java
@@ -1,10 +1,17 @@
 package ca.nyiyui.koseihoryuen;
 
+import ca.nyiyui.koseihoryuen.data.Daishi;
+import ca.nyiyui.koseihoryuen.data.Line;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 
 /**
  * Names: Ivy & Ken
@@ -18,11 +25,26 @@ public class Reberu2 extends ScreenAdapter2 implements PlayableScreen {
     private Stage stage;
     private PlayScreen playScreen;
     private Texture bg;
+    private boolean exploring;
+    private Daishi daishi;
+    /**
+     * Index of line to currently show.
+     */
+    private int curLineIndex;
+    private Telop telop;
 
     public Reberu2(Koseihoryuen game) {
         super(game);
         stage = new Stage(new FillViewport(game.camera.viewportWidth, game.camera.viewportHeight, game.camera), game.batch);
+        try {
+            loadDaishi();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("that's a youproblem ;)");
+        }
+        switchLine(0);
         bg = new Texture(Gdx.files.internal("images/stage2-bg.png"));
+        exploring = false;
     }
 
     @Override
@@ -32,7 +54,25 @@ public class Reberu2 extends ScreenAdapter2 implements PlayableScreen {
 
     @Override
     public void show() {
-        super.show();
+        Gdx.input.setInputProcessor(new InputAdapter() {
+            @Override
+            public boolean keyDown(int keycode) {
+                switch (keycode) {
+                    case Input.Keys.SPACE:
+                    case Input.Keys.ENTER:
+                        switchLine(curLineIndex + 1);
+                        if (curLineIndex >= daishi.lines.size()) {
+                            playScreen.invokePause();
+                            throw new RuntimeException("not impld yet");
+                        }
+                        break;
+                    case Input.Keys.ESCAPE:
+                        playScreen.invokePause();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     @Override
@@ -41,9 +81,42 @@ public class Reberu2 extends ScreenAdapter2 implements PlayableScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
         game.batch.draw(bg, 0, 0);
-        // TODO: put instructions here when dialogue is written
+        if (exploring) {
+
+        } else {
+
+        }
 
 
         game.batch.end();
+    }
+
+    private Line curLine() {
+        return daishi.lines.get(curLineIndex);
+    }
+
+    private void switchLine(int newLineIndex) {
+        curLineIndex = newLineIndex;
+        Line cl = curLine();
+        telop = new Telop(game);
+        telop.setBodyText(cl.body);
+        telop.setTenText(cl.ten);
+        if (cl.action != null)
+            switch (cl.action) {
+                case "explore":
+                    exploring = true;
+                    break;
+            }
+    }
+
+    public void hide() {
+        Gdx.input.setInputProcessor(null);
+    }
+
+    private void loadDaishi() throws IOException {
+        ObjectMapper om = new ObjectMapper();
+        daishi = om.readValue(Gdx.files.internal("daishi/reberu2.json").read(), Daishi.class);
+        for (int i = 1; i < daishi.lines.size(); i++)
+            daishi.lines.get(i).applyDefault(daishi.lines.get(i - 1));
     }
 }
