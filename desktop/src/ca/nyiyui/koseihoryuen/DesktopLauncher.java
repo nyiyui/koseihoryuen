e43040e6ae4e0d58651a99c7cc4fa7c1748d9f52
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-06-04 11:47:01 -0400

    made window non-resizable

--- desktop/src/ca/nyiyui/koseihoryuen/DesktopLauncher.java
+++ desktop/src/ca/nyiyui/koseihoryuen/DesktopLauncher.java
@@ -2,17 +2,17 @@ package ca.nyiyui.koseihoryuen;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import ca.nyiyui.koseihoryuen.Koseihoryuen;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setWindowedMode(960,780);
-		config.useVsync(true);
-		config.setTitle("koseihoryuen");
-		config.setForegroundFPS(60);
-		new Lwjgl3Application(new Koseihoryuen(), config);
-	}
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        config.setWindowedMode(960, 780);
+        config.useVsync(true);
+        config.setTitle("koseihoryuen");
+        config.setForegroundFPS(60);
+        config.setResizable(false);
+        new Lwjgl3Application(new Koseihoryuen(), config);
+    }
 }
