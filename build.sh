a03fba008746a3fe8109edf5c1ff5c4b690a5027
nyiyui <+@nyiyui.ca>
2023-06-08 16:53:21 -0400

    yes

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+java -jar './packr-all-4.0.0.jar' \
+  --platform 'windows64' \
+  --jdk './OpenJDK15U-jdk_x64_windows_hotspot_15.0.2_7.zip' \
+  --useZgcIfSupportedOs \
+  --executable 'koseihoryuen' \
+  --classpath 'desktop/build/libs/desktop-1.0.jar' \
+  --mainclass 'ca.nyiyui.koseihoryuen.DesktopLauncher' \
+  --vmargs 'Xmx1G' \
+  --resources 'assets' \
+  --output 'out-windows64'
